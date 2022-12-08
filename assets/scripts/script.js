8c087c53acd79f2b469e897c2ac94cca41047408
Vitor Sanches Ferreira <112482028+SanchesVitor@users.noreply.github.com>
2022-12-08 02:38:41 -0300

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,7 +24,7 @@ function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
   const carousel = document.querySelector('.s-cards-carousel');
   const transform = carousel.style.transform;
-  const rotateY = transform.match(​/rotateY\((-?\d+deg)\)/i);
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
   const rotateYDeg = -120 * (Number(selectedItem) - 1);
   const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
 
